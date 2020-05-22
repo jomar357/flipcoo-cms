66e826fedf420c6d106f7bc2b7c52ee41ba61014
Virtua Bear Dev <virtuabear@gmail.com>
2020-05-22 19:39:39 +0800

    Replaced spaceId and accessToken

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,27 +1,4 @@
-require('dotenv').config({
-  path: `.env.${process.env.NODE_ENV}`,
-})
 
-const contentfulConfig = {
-  spaceId: process.env.CONTENTFUL_SPACE_ID,
-  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
-}
-
-// if you want to use the preview API please define
-// CONTENTFUL_HOST in your environment config
-// the `host` property should map to `preview.contentful.com`
-// https://www.contentful.com/developers/docs/references/content-preview-api/#/reference/spaces/space/get-a-space/console/js
-if (process.env.CONTENTFUL_HOST) {
-  contentfulConfig.host = process.env.CONTENTFUL_HOST
-}
-
-const { spaceId, accessToken } = contentfulConfig
-
-if (!spaceId || !accessToken) {
-  throw new Error(
-    'Contentful spaceId and the access token need to be provided.'
-  )
-}
 
 module.exports = {
   siteMetadata: {
@@ -44,7 +21,10 @@ module.exports = {
     'gatsby-plugin-sharp',
     {
       resolve: 'gatsby-source-contentful',
-      options: contentfulConfig,
+      options: {
+        spaceId: 'yykcyof9d68d',
+        accessToken: 'ieoT5PZ5Yu71lBTL0B_PrQPhWdjrtTmuncHQEkAyBhM',
+      },
     },
     {
       resolve: `gatsby-plugin-manifest`,
